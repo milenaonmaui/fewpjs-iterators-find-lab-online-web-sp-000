570c2e482e799a0a40cab86711cf44369d14f006
Milena Kari <milenakari@yahoo.com>
2019-10-23 01:21:45 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(arr){
+  let res = arr.find(s => s.result === "W" )
+  if (res) {
+    return res.year
+  } else {
+    return undefined
+  }
+}
